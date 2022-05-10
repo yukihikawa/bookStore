8df3124ed6b1cb79241ec82bfaa1662984035361
wurifu desktop <rifuwu123@gmail.com>
2022-05-10 11:46:16 +0800

    Initial commit

---
+++ src/main/com/wrf/test/HelloServlet.java
@@ -0,0 +1,23 @@
+package wrf.test;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.Writer;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Writer ws = response.getWriter();
+        ws.write("Sadfad");
+        ws.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Writer ws = response.getWriter();
+        ws.write("Sadfasf");
+        ws.flush();
+    }
+}
